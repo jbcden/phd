46ac0dda94003f25a8580a2051ecf7019099e630
Jacob Chae <jbcden@gmail.com>
2012-11-04 12:12:13 -0500

    inital commit take2...

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Phd::Application.config.secret_token = '015e089075e1e0e603a90f87f15987fe7865ee8a88f5b6899ae8b3483775e934040e89083cc684d962291ac31c7280bc95edf4c246a272d2a1f39b6d68f8f8c6'
