fb4b5cb93891b5c81f0880846e8b25107ad5629d
Jacob Chae <jbcden@gmail.com>
2012-11-04 20:15:51 -0500

    Basic Home Page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  #Produces a title for each page.
+  def title(page_title)
+    base = "PHD"
+    if page_title.empty?
+      base
+    else
+      "#{base} | #{page_title}"
+    end
+  end
 end
